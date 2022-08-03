78f262698a0f091c389125304790b4767ba891ec
atikis87 <ati.radeone@gmail.com>
2022-08-03 14:03:15 +0200

    add new buttons

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+
 const app = express();
-
 //Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join('public')));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
