b42ba1904d97828f3f22f75295b49363292aacb9
atikis87 <ati.radeone@gmail.com>
2022-08-04 17:19:05 +0200

    add responsive solution, new footer, and new background

--- README.md
+++ README.md
@@ -1,26 +1,17 @@
-# The_Offer_Website
+# The_Offer_Website Coding Challenge
 
-# To-Dos
-    * Please create a simple landing page interacting with the proposed API.
-    * Visually the page consists of header, main part and footer.
-        * Header has dummy logo and and few items of navigation (some external links to everything you like) 
-        * Main part contains:
-            * Dummy headline(s)
-            * 2-3 sentences of marketing text (Lorem Ipsum..)
-            * Input field to enter the address
-            * Offers, which appear after user enters the address.
-        * Footer includes dummy links to imprint, contact page, as well as few links to social networks, styled as icons.
-    * Input field has Reset button
-    * Input field some basic validation
-    * Display offers in something like a panel or card each
-    * Each offer card contains:
-        * name of offer
-        * price
-        * small expandable description text (e.g. up to 10 Lorem Ipsum words), unique for each offer
-        * button to offer details (any external link)
-    * Sort the offers based on price and start with the most expensive one.
-    * Highlight the first offer somehow (e.g. using a special color/label/icon) but only if more than one offer is provided.
-    * Simulate random delay (up to 3 seconds) for loading offers via API. Visualise the process somehow, e.g. showing skeleton loading.
-    * Think about appropriate error handling, semantics, accessibility, performance, SEO etc.
-    * Use mobile-first approach. Make sure, your page looks solid on smartphone/tablet/desktop.
-    * Use any fonts, colors, images you like.
+This project was created using HTML CSS and Vanilla JS. I only used framework for the server.
+
+## Available Scripts
+
+The website starts from app.js.
+
+### `Getting Started`
+
+1.  `npm install`
+2.  `npm run dev`
+3.  `npm start`
+
+
+Runs the app in the development mode.<br>
+Open [http://localhost:5000](http://localhost:5000) to view it in the browser.
--- public/js/apiHandler.js
+++ public/js/apiHandler.js
@@ -1,37 +1,54 @@
 const API_PATH = "./data/api.json";
-const getOffer = document.querySelector(".offer-container");
-const loadingElement = document.querySelector('.loading');
-const addressButton = document.querySelector('.go-button');
-
-loadingElement.style.display = 'none';
+const offersElement = document.querySelector('.offer-container');
+const loadingElement = document.querySelector('.skeleton');
+const offersButton = document.querySelector('.submit-btn');
 
 
-async function getData(){
-    getOffer.innerHTML = '';
+async function getOffers() {
+    console.log("Szopd ki bobi2");
+    offersElement.innerHTML = '';
+    console.log("Szopd ki bobi3");
     loadingElement.style.display = '';
     const response = await fetch(API_PATH);
     const json = await response.json();
-    console.log(json)
 
-    json.message.forEach(dogImage => {
-        getOffer.innerHTML += `
-        <div class="offer flex-column active-offer">
-            <h3 class="offer-name"></h3>
-            <h4 class="offer-description">
-            </h4>
-            <div class="offer-price-container flex">
-                <h2 class="offer-price">39.00 €</h2>
-            </div>
-            <a href="#" class="offer-button">More</a>
+    json.forEach(Offers => {
+      offersElement.innerHTML += `
+      <div class="offer flex-column active-offer">
+        <h3 class="offer-name">Super Fast</h3>
+        <h4 class="offer-description">
+        The fastest internet connection ever!
+        </h4>
+        <div class="offer-price-container flex">
+            <h2 class="offer-price">39.00 €</h2>
         </div>
+        <a href="#" class="offer-button">More</a>
+      </div>
+      `;
+    });
+    loadingElement.style.display = 'none';
+  }
+  getOffers() 
+  offersButton.addEventListener('click', getOffers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
-            `;
-        }
-    )
-};
-
-getData();
-
 
 /*
     <div class="offer flex-column active-offer">
@@ -44,28 +61,4 @@ getData();
       </div>
       <a href="#" class="offer-button">More</a>
     </div>
-
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-fetch(API_PATH).then(function (response) {
-    return response.json();
-    }).then(function (data) {
-    document.getElementById("api").innerHTML = JSON.stringify( data, null, 4);
-    }).catch(function (err) {
-    console.log('error: ' + err);
-    });
-
 */
